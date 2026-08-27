62141765f7cc45e0adbd0541446a411306ff65df
Amit <amit4college@gmail.com>
2026-08-27 05:08:06 +0000

    changees

--- practical2-d/arrayMethods.js
+++ practical2-d/arrayMethods.js
@@ -8,4 +8,3 @@ console.log("Greater than 20 : ", filtered);
 
 let sum = numbers.reduce((acc, curr) => acc + curr, 0);
 console.log("Sum of all numbers:", sum);
-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
